6ae0ba14721cbb3072bebfb8a02445e72cc12b2e
zcrumpton <z.crumpton@gmail.com>
2020-12-01 21:07:01 -0600

    models are completed for backend

---
+++ LocalBuzz_BackEndCapstone/Model/Artist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalBuzz_BackEndCapstone.Model
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string ArtistName { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Genre { get; set; }
+        public string ArtistPhoto { get; set; }
+        public int Followers { get; set; }
+        public bool isArtist { get; set; }
+        
+
+    }
+}
---
+++ LocalBuzz_BackEndCapstone/Model/Events.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalBuzz_BackEndCapstone.Model
+{
+    public class Events
+    {
+        public int EventId { get; set; }
+        public int AId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public DateTime Date { get; set; }
+        public int TicketPrice { get; set; }
+    }
+}
