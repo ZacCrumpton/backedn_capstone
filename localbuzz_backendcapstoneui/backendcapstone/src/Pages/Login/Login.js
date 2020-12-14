fec839a87a2d81be1b9cc7af19dfb999d9376139
zcrumpton <z.crumpton@gmail.com>
2020-12-13 23:37:09 -0600

    added bearbones for login. wip

--- localbuzz_backendcapstoneui/backendcapstone/src/Pages/Login/Login.js
+++ localbuzz_backendcapstoneui/backendcapstone/src/Pages/Login/Login.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import './Login.scss';
+import PropTypes from 'prop-types';
+// need to set state for "isArtist"
+// is Artist will determine if user is an Artist or normal user
+// needs to load a form based on what the user chooses.
 
 class Login extends React.Component {
-  render() {
-    return (
+  static propTypes = {
+    isArtist: PropTypes.bool.isRequired,
+  }
+
+    state = {
+      isArtist: false,
+      isOpen: false,
+    }
+
+    toggle = () => {
+      this.setState({ isOpen: !this.state.isOpen });
+    }
+
+    logOut = (e) => {
+      e.preventDefault();
+      // firebase.auth().signout();
+    }
+
+    render() {
+      return (
             <div>
                 <div id="LoginContainer" className="card">
                     <h5 className="card-header logHeader">Login</h5>
@@ -17,8 +39,8 @@ class Login extends React.Component {
                     </button>
                 </div>
             </div>
-    );
-  }
+      );
+    }
 }
 
 export default Login;
